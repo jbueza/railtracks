badc79613dcdd03304c13263ddcd8a33c0f3c387
Logan <logan@railtown.ai>
2025-01-24 08:54:50 -0800

    more docstring

--- src/railtownai_rc/llm/tools.py
+++ src/railtownai_rc/llm/tools.py
@@ -41,15 +41,22 @@ class Parameter:
 
 
 class Tool:
+    """
+    A quasi immutable class designed to represent a single Tool object. This tool should be inserted into a model
+    to allow the model to call the tool during generation.
 
-    def __init__(self, name: str, detail: str = "", parameters: Optional[Type[BaseModel]] = None):
+    You pass in the key details in terms of the name, a short description, and a base model representing the parameters
+    the model requires.
+    """
+
+    def __init__(self, name: str, detail: str, parameters: Optional[Type[BaseModel]] = None):
         """
         Creates a new instance of a tool object.
 
         Args:
             name: The name of the tool.
             detail: A detailed description of the tool.
-            parameters: The parameters attached to this tool.
+            parameters: The parameters attached to this tool. If none, then there is no parameters for this model.
         """
         self._name = name
         self._detail = detail
@@ -61,11 +68,16 @@ class Tool:
 
     @property
     def detail(self):
+        """
+        Returns the string message attached to the detail of this tool
+        """
         return self._detail
 
     @property
     def parameters(self):
-        # pass by value of the list entries.
+        """
+        Gets the parameters attached to this tool. If there are no parameters, this will return None.
+        """
         return self._parameters
 
     def __str__(self):
@@ -75,3 +87,5 @@ class Tool:
     def from_function(cls, function: Callable) -> Self:
         # TODO: complete the specialized logic. See github issue.
         pass
+
+    # TODO: add a method to convert a node into a tool.
