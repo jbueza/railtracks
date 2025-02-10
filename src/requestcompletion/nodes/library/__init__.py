f4e40bc561ae0f3a9fb132124b8cdc0fc2faf2fd
Logan <logan@railtown.ai>
2025-02-10 12:35:29 -0800

    added structured llm to import path

--- src/requestcompletion/nodes/library/__init__.py
+++ src/requestcompletion/nodes/library/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ["FunctionNode", "TerminalLLM", "ToolCallLLM", "MessageHistoryToolCallLLM"]
+__all__ = ["FunctionNode", "TerminalLLM", "ToolCallLLM", "MessageHistoryToolCallLLM", "StructuredLLM"]
 
 from .function import FunctionNode
 from .terminal_llm import TerminalLLM
 from .tool_call_llm import ToolCallLLM
 from .mess_hist_tool_call_llm import MessageHistoryToolCallLLM
+from .structured_llm import StructuredLLM
