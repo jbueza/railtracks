24d77e88cd8de89d5c1e3daae0cb8af408c46e28
Logan <logan@railtown.ai>
2025-03-19 18:47:15 -0700

    added tests for inputs

--- tests/rc_tests/run/test_executor.py
+++ tests/rc_tests/run/test_executor.py
@@ -3,9 +3,12 @@ from __future__ import annotations
 import asyncio
 import random
 import time
+from typing import Dict, Any
 
 import requestcompletion as rc
+from typing_extensions import Self
 
+from src.requestcompletion.state.request import RequestTemplate
 
 RNGNode = rc.library.from_function(random.random)
 
@@ -14,14 +17,6 @@ async def many_calls(num_calls: int, parallel_calls: int):
     data = []
     for _ in range(num_calls):
         contracts = [rc.call(RNGNode) for _ in range(parallel_calls)]
-        results = await asyncio.gather(*contracts)
-        data.extend(results)
-    return data
-
-async def nested_many_calls(num_calls: int, parallel_calls: int, depth: int):
-    data = []
-    for _ in range(num_calls):
-        contracts = [rc.call(many_calls, parallel_calls, 1) for _ in range(parallel_calls)]
         results = await asyncio.gather(*contracts)
         data.extend(results)
     return data
@@ -67,3 +62,82 @@ def test_simple_rng():
         result = run.run_sync(RNGNode)
 
     assert 0 < result.answer < 1
+
+
+class NestedManyCalls(rc.Node):
+
+    def __init__(self, num_calls: int, parallel_calls: int, depth: int):
+        self.num_calls = num_calls
+        self.parallel_calls = parallel_calls
+        self.depth = depth
+        super().__init__()
+
+    async def invoke(
+        self,
+    ):
+        data = []
+        for _ in range(self.num_calls):
+            if self.depth == 0:
+                contracts = [rc.call(RNGNode) for _ in range(self.parallel_calls)]
+                results = await asyncio.gather(*contracts)
+
+            else:
+                contracts = [
+                    rc.call(NestedManyCalls, self.num_calls, self.parallel_calls, self.depth - 1)
+                    for _ in range(self.parallel_calls)
+                ]
+
+                results = await asyncio.gather(*contracts)
+                # flatten the list here.
+                results = [x for y in results for x in y]
+            data.extend(results)
+        return data
+
+    @classmethod
+    def pretty_name(cls) -> str:
+        return "NestedManyCalls"
+
+
+def nested_many_calls_tester(num_calls: int, parallel_calls: int, depth: int):
+    with rc.Runner() as run:
+        finished_result = run.run_sync(NestedManyCalls, num_calls, parallel_calls, depth)
+
+    ans = finished_result.answer
+
+    assert isinstance(ans, list)
+    assert len(ans) == (parallel_calls * num_calls) ** (depth + 1)
+    assert all([0 < x < 1 for x in ans])
+
+    r_h = finished_result.request_heap
+    child_requests = r_h.children(r_h.insertion_request.sink_id)
+
+    assert len(child_requests) == num_calls * parallel_calls
+    print(child_requests[0].input)
+    for r in child_requests:
+        assert r.input[0][0] == num_calls
+        assert r.input[0][1] == parallel_calls
+        assert 0 < r.input[0][2] < depth
+
+
+def test_nested_no_deadlock():
+    num_calls = 2
+    parallel_calls = 2
+    depth = 3
+
+    nested_many_calls_tester(num_calls, parallel_calls, depth)
+
+
+def test_nested_no_deadlock_harder():
+    num_calls = 1
+    parallel_calls = 3
+    depth = 3
+
+    nested_many_calls_tester(num_calls, parallel_calls, depth)
+
+
+def test_nested_no_deadlock_harder_2():
+    num_calls = 3
+    parallel_calls = 1
+    depth = 3
+
+    nested_many_calls_tester(num_calls, parallel_calls, depth)
