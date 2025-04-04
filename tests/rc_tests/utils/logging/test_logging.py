fd229b8dd8f93429e7ee5e3b0a0d0ce1f004c115
Logan <logan@railtown.ai>
2025-04-04 12:42:51 -0700

    added tests for logging

---
+++ tests/rc_tests/utils/logging/test_logging.py
@@ -0,0 +1,70 @@
+import random
+import asyncio
+
+import pytest
+import requestcompletion as rc
+
+
+RNGNode = rc.library.from_function(random.random)
+
+
+async def top_level(number_of_calls: int):
+
+    contracts = [rc.call(RNGNode) for _ in range(number_of_calls)]
+
+    random_numbers = await asyncio.gather(*contracts)
+
+    return random_numbers
+
+
+TopLevelNode = rc.library.from_function(top_level)
+
+
+def test_simple_request(caplog):
+    with rc.Runner() as run:
+        result = run.run_sync(RNGNode)
+
+    # one for start and one for end
+    assert len(caplog.messages) == 2
+    assert caplog.messages[0] == "START CREATED random Node - (, )"
+    assert caplog.messages[1] == f"random Node DONE {result.answer}"
+
+    assert isinstance(result.answer, float)
+
+
+def test_more_complex_request(caplog):
+    subcalls = 10
+    with rc.Runner() as run:
+        result = run.run_sync(TopLevelNode, subcalls)
+
+    assert len(caplog.messages) == subcalls * 2 + 2
+    assert caplog.messages[0] == "START CREATED top_level Node - (10, )"
+    assert caplog.messages[1] == "top_level Node CREATED random Node - (, )"
+    assert caplog.messages[-1] == f"top_level Node DONE {result.answer}"
+
+
+def test_more_complex_request_regular(caplog):
+    subcalls = 10
+    with rc.Runner(executor_config=rc.ExecutorConfig(logging_setting="REGULAR")) as run:
+        result = run.run_sync(TopLevelNode, subcalls)
+
+    assert len(caplog.messages) == subcalls * 2 + 2
+    assert caplog.messages[0] == "START CREATED top_level Node - (10, )"
+    assert caplog.messages[1] == "top_level Node CREATED random Node - (, )"
+    assert caplog.messages[-1] == f"top_level Node DONE {result.answer}"
+
+
+def test_more_complex_request_regular_quiet(caplog):
+    subcalls = 10
+    with rc.Runner(executor_config=rc.ExecutorConfig(logging_setting="QUIET")) as run:
+        result = run.run_sync(TopLevelNode, subcalls)
+
+    assert len(caplog.messages) == 0
+
+
+def test_more_complex_request_regular_none(caplog):
+    subcalls = 10
+    with rc.Runner(executor_config=rc.ExecutorConfig(logging_setting="NONE")) as run:
+        result = run.run_sync(TopLevelNode, subcalls)
+
+    assert len(caplog.messages) == 0
