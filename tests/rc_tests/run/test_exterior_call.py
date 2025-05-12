0365db97d738b9f33c41466d91209f4c5610f191
Logan <logan@railtown.ai>
2025-05-11 17:40:06 -0700

    formatting and deleting sandbox folder

--- tests/rc_tests/run/test_exterior_call.py
+++ tests/rc_tests/run/test_exterior_call.py
@@ -10,5 +10,5 @@ async def test_runner_call_basic():
     response = await rc.call(RNGNode)
     assert isinstance(response, float), "Expected a float result from RNGNode"
 
-# TODO write tests for accessing state and setting state after and before the fact.
 
+# TODO write tests for accessing state and setting state after and before the fact.
