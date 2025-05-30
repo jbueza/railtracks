28a45c339a8dd21ff8e02d3a2e0150c3cf2e8e52
Logan <logan@railtown.ai>
2025-05-30 15:47:04 -0700

    initial changes

--- tests/rc_tests/utils/logging/test_logging.py
+++ tests/rc_tests/utils/logging/test_logging.py
@@ -5,6 +5,7 @@ import pytest
 import requestcompletion as rc
 import re
 
+from src.requestcompletion import ExecutorConfig
 
 RNGNode = rc.library.from_function(random.random)
 
@@ -13,8 +14,8 @@ def strip_ansi_colors(message: str) -> str:
     """
     Removes ANSI color codes from the given message.
     """
-    ansi_escape = re.compile(r'\x1b\[[0-9;]*m')
-    return ansi_escape.sub('', message)
+    ansi_escape = re.compile(r"\x1b\[[0-9;]*m")
+    return ansi_escape.sub("", message)
 
 
 async def top_level(number_of_calls: int):
@@ -30,7 +31,7 @@ TopLevelNode = rc.library.from_function(top_level)
 
 
 def test_simple_request(caplog):
-    with rc.Runner() as run:
+    with rc.Runner(executor_config=rc.ExecutorConfig(logging_setting="REGULAR")) as run:
         result = run.run_sync(RNGNode)
 
     stripped_messages = [strip_ansi_colors(msg) for msg in caplog.messages]
@@ -45,7 +46,7 @@ def test_simple_request(caplog):
 
 def test_more_complex_request(caplog):
     subcalls = 10
-    with rc.Runner() as run:
+    with rc.Runner(executor_config=rc.ExecutorConfig(logging_setting="REGULAR")) as run:
         result = run.run_sync(TopLevelNode, subcalls)
 
     stripped_messages = [strip_ansi_colors(msg) for msg in caplog.messages]
