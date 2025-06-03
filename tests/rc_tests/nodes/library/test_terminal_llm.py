d2c0a732ea618e62a31145a30288099c6c0f3db4
Logan <logan@railtown.ai>
2025-06-03 11:47:20 -0700

    turned off logging for errors

--- tests/rc_tests/nodes/library/test_terminal_llm.py
+++ tests/rc_tests/nodes/library/test_terminal_llm.py
@@ -88,9 +88,7 @@ async def test_terminal_llm_as_tool_correct_initialization(
         system_message=system_randomizer,
     )
 
-    with rc.Runner(
-        executor_config=rc.ExecutorConfig(logging_setting="NONE")
-    ) as runner:
+    with rc.Runner(executor_config=rc.ExecutorConfig(logging_setting="NONE")) as runner:
         message_history = rc.llm.MessageHistory(
             [rc.llm.UserMessage("The input string is 'hello world'")]
         )
--- tests/rc_tests/run/test_multiple_runners.py
+++ tests/rc_tests/run/test_multiple_runners.py
@@ -28,6 +28,7 @@ def test_sync_runners_w_executor():
         for m in mapped_results:
             assert isinstance(m, float), "Expected a float result from RNGNode"
 
+
 @pytest.mark.asyncio
 async def test_async_runners_w_async():
     async def run_rng(timeout_len: float):
@@ -65,4 +66,3 @@ def nested_runner_call():
 
 
 NestedRunner = rc.library.from_function(nested_runner_call)
-
