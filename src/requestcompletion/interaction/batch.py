d4c4350af93dcddefdbb1b2ccc26b3ce9f7706c8
Logan <logan@railtown.ai>
2025-06-05 13:50:23 -0700

    added better logic for testing of rc batch and new logic to pass in return exceptions

--- src/requestcompletion/interaction/batch.py
+++ src/requestcompletion/interaction/batch.py
@@ -22,18 +22,26 @@ _TOutput = TypeVar("_TOutput")
 async def batch(
     node: Callable[..., Node[_TOutput]],
     *iterables: Iterable[Any],
+    return_exceptions: bool = True,
 ):
     """
     Complete a node over multiple iterables, allowing for parallel execution.
 
     Note the results will be returned in the order of the iterables, not the order of completion.
 
+    If one of the nodes returns an exception, the thrown exception will included as a response.
+
     Args:
         node: The node type to create.
         *iterables: The iterables to map the node over.
+        return_exceptions: If True, exceptions will be returned as part of the results.
+            If False, exceptions will be raised immediately and you will lose access to the results.
+            Defaults to true.
 
     Returns:
         An iterable of results from the node.
     """
     contracts = [call(node, *args) for args in zip(*iterables)]
-    return await asyncio.gather(*contracts)
+
+    results = await asyncio.gather(*contracts, return_exceptions=return_exceptions)
+    return results
