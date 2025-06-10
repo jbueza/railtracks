1633eee155980400b4a4f06638c96032665d3518
Levente Varsanyi <108640413+Levivus@users.noreply.github.com>
2025-06-10 16:34:53 -0700

    bug fix

--- src/requestcompletion/mcp/__init__.py
+++ src/requestcompletion/mcp/__init__.py
@@ -1,5 +1,5 @@
 from .to_node import create_mcp_server
-from .mcp_utils import MCPHttpParams, StdioServerParameters
+from .main import MCPHttpParams, StdioServerParameters
 
 __all__ = [
     "create_mcp_server",
